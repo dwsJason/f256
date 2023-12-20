3f064686f8c46daf9e999a8a01e92b806d300f98
JASON-6700K\jandersen <JGAndersen@gmail.com>
2023-12-20 14:51:33 -0500

    xmas: dumb thing to keep my merlin32 from crashing

--- merlin32/xmas/link.s
+++ merlin32/xmas/link.s
@@ -47,7 +47,7 @@ main_code_start
 		put mod.s
 		put mixer.s
 		put version.s   ; hardware version checker
-		put flame.s
+		put flame.s     ; this comment stops merlin32 crash
 main_code_end
 
 ;------------------------------------------------------------------------------
