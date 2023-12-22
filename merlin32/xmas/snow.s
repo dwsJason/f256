df8ac7038efa87d919469981d59762d81516d20e
JASON-6700K\jandersen <JGAndersen@gmail.com>
2023-12-22 08:59:22 -0500

    xmas:  let it snow

--- merlin32/xmas/snow.s
+++ merlin32/xmas/snow.s
@@ -91,12 +91,149 @@ SnowInit
 		dex
 		bpl ]lp
 
+;
+; Initial Snow BG positions
+;
+		ldax #0+SNOWMAP_SIZE_Y-240
+		stax snow_bg_y+1
+		stax snow_fg_y+1
+
+		ldax #320
+		stax snow_bg_x+1
+		stax snow_fg_x+1
+
 		rts
 
 ;------------------------------------------------------------------------------
-
+;
+;
+;
 SnowPump
+		lda io_ctrl
+		pha
+		stz io_ctrl
+
+; Step one, reflect registers into HW
+; and pray we're in VBlank
+		ldax snow_bg_x+1
+		stax VKY_TM1_POS_X_L
+		ldax snow_bg_y+1
+		stax VKY_TM1_POS_Y_L
+
+		ldax snow_fg_x+1
+		stax VKY_TM2_POS_X_L
+		ldax snow_fg_y+1
+		stax VKY_TM2_POS_Y_L
+
+;
+; Now Animate the Planes, by applying some velocity to the positions
+;
+		sec
+		lda snow_bg_y
+		sbc #<SNOW_BG_VY
+		sta snow_bg_y
+		lda snow_bg_y+1
+		sbc #>SNOW_BG_VY
+		sta snow_bg_y+1
+		lda snow_bg_y+2
+		sbc #0
+		sta snow_bg_y+2
+
+		clc
+		lda snow_bg_x
+		adc #<SNOW_BG_VX
+		sta snow_bg_x
+		lda snow_bg_x+1
+		adc #>SNOW_BG_VX
+		sta snow_bg_x+1
+		lda snow_bg_x+2
+		adc #0
+		sta snow_bg_x+2
+
+		sec
+		lda snow_fg_y
+		sbc #<SNOW_FG_VY
+		sta snow_fg_y
+		lda snow_fg_y+1
+		sbc #>SNOW_FG_VY
+		sta snow_fg_y+1
+		lda snow_fg_y+2
+		sbc #0
+		sta snow_fg_y+2
+
+		clc
+		lda snow_fg_x
+		adc #<SNOW_FG_VX
+		sta snow_fg_x
+		lda snow_fg_x+1
+		adc #>SNOW_FG_VX
+		sta snow_fg_x+1
+		lda snow_fg_x+2
+		adc #0
+		sta snow_fg_x+2
+
+
+
+;
+; Now Clamp the coordinates for perfect wrapping
+;
+
+		ldax snow_bg_y+1
+		cmpax #32
+		bcs :no_y_wrap
+		; wrap the snow
+		adc #240
+		sta snow_bg_y+1
+		lda snow_bg_y+2
+		adc #0
+		sta snow_bg_y+2
+:no_y_wrap
+
+		ldax snow_bg_x+1
+		cmpax #320
+		bcc :no_x_wrap
+		; wrap the snow
+		sbc #<320
+		sta snow_bg_x+1
+		lda snow_bg_x+2
+		sbc #>320
+		sta snow_bg_x+2
+:no_x_wrap
+
+
+		ldax snow_fg_y+1
+		cmpax #32
+		bcs :fgno_y_wrap
+		; wrap the snow
+		adc #240
+		sta snow_fg_y+1
+		lda snow_fg_y+2
+		adc #0
+		sta snow_fg_y+2
+:fgno_y_wrap
+
+
+		ldax snow_fg_x+1
+		cmpax #320
+		bcs :fgno_x_wrap
+		; wrap the snow
+		sbc #<320
+		sta snow_fg_x+1
+		lda snow_fg_x+2
+		sbc #>320
+		sta snow_fg_x+2
+:fgno_x_wrap
+
+		pla
+		sta io_ctrl
+
 		rts
+
+snow_bg_x ds 3
+snow_bg_y ds 3
+
+snow_fg_x ds 3
+snow_fg_y ds 3
 
 ;------------------------------------------------------------------------------
 
